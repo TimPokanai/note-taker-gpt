a0541fcd565fc85298c0e1cbe3aeb9580b877cf6
Timothy Pokanai <pokanait@mcmaster.ca>
2025-07-16 19:38:35 -0400

    Completed new audio recording backend

--- record_audio.py
+++ record_audio.py
@@ -1,7 +1,12 @@
 import threading
+import time
+import queue
+import os
 
 import sounddevice as sd
+import soundfile as sf
 from scipy.io.wavfile import write
+from transcribe_audio import transcribe_audio
 
 MAX_DURATION = 60 * 50 # We can record sessions for up to 50 minutes
 SAMPLE_RATE = 16000
@@ -10,14 +15,35 @@ WAV_OUTPUT = "lecture.wav"
 
 recording = True
 
-def record_audio(filename="lecture.wav", duration=30, fs=16000):
-    print("Recording...")
+audio_queue = queue.Queue()
 
-    audio = sd.rec(int(duration * fs), samplerate=fs, channels=1, dtype="int16")
-    sd.wait()
-    write(filename, fs, audio)
+# def record_audio(filename="lecture.wav", duration=30, fs=16000):
+#     print("Recording...")
 
-    print(f"Saved to {filename}")
+#     audio = sd.rec(int(duration * fs), samplerate=fs, channels=1, dtype="int16")
+#     sd.wait()
+#     write(filename, fs, audio)
+
+#     print(f"Saved to {filename}")
+
+def audio_callback(indata, frames, time, status):
+    audio_queue.put(indata.copy())
+
+def record_audio():
+
+    if os.path.exists(WAV_OUTPUT):
+        print(f"{WAV_OUTPUT} already exists!")
+        os.remove(WAV_OUTPUT)
+
+    with sf.SoundFile(WAV_OUTPUT, mode="x", samplerate=SAMPLE_RATE, channels=CHANNELS, subtype="PCM_16") as file:
+        with sd.InputStream(samplerate=SAMPLE_RATE, channels=CHANNELS, callback=audio_callback):
+            print("Recording has started. Press ENTER to stop early.")
+            start_time = time.time()
+
+            while recording and (time.time() - start_time < MAX_DURATION):
+                file.write(audio_queue.get())
+
+            print("Recording has stopped.")
 
 def start_recording():
     global recording
@@ -29,3 +55,12 @@ def start_recording():
     input("Press ENTER to stop recording\n")
     recording = False
     recording_thread.join()
+
+def process():
+    transcript = transcribe_audio(WAV_OUTPUT)
+    print("Transcription complete.")
+    print(transcript)
+
+if __name__ == "__main__":
+    start_recording()
+    process()
