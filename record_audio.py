9896c3fb8724ad40b911efc0f71bd2990330fcd4
Timothy Pokanai <pokanait@mcmaster.ca>
2025-07-16 19:16:46 -0400

    Started logic for more efficient backend

--- record_audio.py
+++ record_audio.py
@@ -1,7 +1,16 @@
+import threading
+
 import sounddevice as sd
 from scipy.io.wavfile import write
 
-def record_audio(filename="lecture.wav", duration=300, fs=16000):
+MAX_DURATION = 60 * 50 # We can record sessions for up to 50 minutes
+SAMPLE_RATE = 16000
+CHANNELS = 1
+WAV_OUTPUT = "lecture.wav"
+
+recording = True
+
+def record_audio(filename="lecture.wav", duration=30, fs=16000):
     print("Recording...")
 
     audio = sd.rec(int(duration * fs), samplerate=fs, channels=1, dtype="int16")
@@ -9,3 +18,14 @@ def record_audio(filename="lecture.wav", duration=300, fs=16000):
     write(filename, fs, audio)
 
     print(f"Saved to {filename}")
+
+def start_recording():
+    global recording
+    recording = True
+
+    recording_thread = threading.Thread(target = record_audio)
+    recording_thread.start()
+
+    input("Press ENTER to stop recording\n")
+    recording = False
+    recording_thread.join()
